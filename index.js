b4380831f7158c6c6172628235325275694d532a
Pedro Ventura <pedrovnt@gmail.com>
2023-05-30 19:55:57 -0700

    looping

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  const names = ["Alice", "Bob", "Charlie"];
+const messages = writeCards(names);
+console.log(messages);
+
+function writeCards(names, giftType) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${giftType} gift!`);
+  }
